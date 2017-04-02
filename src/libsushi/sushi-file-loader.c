738353df3c6e9b452783b243972a1474777f7b9e
Cosimo Cecchi <cosimoc@gnome.org>
2017-04-02 10:39:24 -0700

    file-loader: fix warnings

--- src/libsushi/sushi-file-loader.c
+++ src/libsushi/sushi-file-loader.c
@@ -583,8 +583,7 @@ sushi_file_loader_get_icon (SushiFileLoader *self)
 
   info = gtk_icon_theme_lookup_by_gicon (gtk_icon_theme_get_default (),
                                          g_file_info_get_icon (self->priv->info),
-                                         256,
-                                         GTK_ICON_LOOKUP_GENERIC_FALLBACK);
+                                         256, 0);
 
   if (info == NULL)
     return NULL;
