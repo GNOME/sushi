GNOME/sushi

commit 6258dc70814638e99d3a604eda1cd7f60ed53ffa

sushi-media-bin: Fix position update on fullscreen
We have to add the tick callback to the overlay widget instead of the main
widget, otherwise the position bar is not updated in fullscreen mode when the
gtksink is used. This happens because we move the overlay between the main and
fullscreen windows, and we did't get the events when the overlay is outside of
the main widget.

src/libsushi/sushi-media-bin.c
@@ -1554,13 +1554,13 @@ sushi_media_bin_tick_callback (GtkWidget     *widget,
 {
   static GstDebugLevel level;
 
-  sushi_media_bin_update_position ((SushiMediaBin *)widget);
+  sushi_media_bin_update_position ((SushiMediaBin *)user_data);
 
   if (level == 0)
     level = gst_debug_category_get_threshold (sushi_media_bin_debug);
 
   if (level >= GST_LEVEL_INFO)
-    log_fps ((SushiMediaBin *)widget, frame_clock);
+    log_fps ((SushiMediaBin *)user_data, frame_clock);
 
   return G_SOURCE_CONTINUE;
 }
@@ -1577,9 +1577,9 @@ sushi_media_bin_set_tick_enabled (SushiMediaBin *self, gboolean enabled)
     }
 
   if (enabled)
-    priv->tick_id = gtk_widget_add_tick_callback (GTK_WIDGET (self),
+    priv->tick_id = gtk_widget_add_tick_callback (priv->overlay,
                                                   sushi_media_bin_tick_callback,
-                                                  NULL, NULL);
+                                                  self, NULL);
 }
 
 static inline void

commit ba9b3033cb7953d7ec8b768876437cb42cb9016c

sushi-media-bin: Fix position update for audio mode
The audio mode doesn't have overlay widget.

Fixes: 6258dc70814638e99d3a604eda1cd7f60ed53ffa

Closes: https://gitlab.gnome.org/GNOME/sushi/-/issues/128

src/libsushi/sushi-media-bin.c
@@ -1577,7 +1577,7 @@ sushi_media_bin_set_tick_enabled (SushiMediaBin *self, gboolean enabled)
     }
 
   if (enabled)
-    priv->tick_id = gtk_widget_add_tick_callback (priv->overlay,
+    priv->tick_id = gtk_widget_add_tick_callback (priv->audio_mode ? priv->audio_box : priv->overlay,
                                                   sushi_media_bin_tick_callback,
                                                   self, NULL);
 }