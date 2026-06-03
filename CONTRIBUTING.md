781a26c2f0c30abb5a74dd6ad447650b92a5e786
Tau Gärtli <git@tau.garden>
2026-06-03 17:07:17 +0200

    main: Port to ES Modules

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -12,7 +12,7 @@ After opening the project in Builder, do the following:
 1. Make sure that your active configuration is `org.gnome.NautilusPreviewer.json`.
 2. Go to "Configure Project..." (<kbd>Ctrl</kbd> + <kbd>,</kbd>) → "Commands"
 3. Create a new command: \
-   **Shell Command:** `gjs /app/libexec/org.gnome.NautilusPreviewerDevel` \
+   **Shell Command:** `/app/libexec/org.gnome.NautilusPreviewerDevel` \
    **Add variable:** `SUSHI_PERSIST=1`
 4. Go to "Application" and change the "Run Command" to your newly created command.
 
--- src/core/main.js
+++ src/core/main.js
@@ -22,37 +22,38 @@
  * Authors: Cosimo Cecchi <cosimoc@redhat.com>
  *
  */
+
+import 'gi://Adw?version=1';
+import 'gi://GLib?version=2.0';
+import 'gi://GObject?version=2.0';
+import 'gi://Gdk?version=4.0';
+import 'gi://Gio?version=2.0';
+import 'gi://GioUnix?version=2.0';
+import 'gi://Gly?version=2';
+import 'gi://GlyGtk4?version=2';
+import 'gi://Gst?version=1.0';
+import 'gi://GstTag?version=1.0';
+import 'gi://Gtk?version=4.0';
+import 'gi://GtkSource?version=5';
+import 'gi://Pango?version=1.0';
+import 'gi://PapersDocument?version=4.0';
+import 'gi://PapersView?version=4.0';
+import 'gi://Soup?version=3.0';
+import 'gi://Sushi?version=1.0';
+
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+
 pkg.initGettext();
 pkg.initFormat();
-pkg.require({
-    Adw: '1',
-    PapersDocument: '4.0',
-    PapersView: '4.0',
-    Gdk: '4.0',
-    Gio: '2.0',
-    GioUnix: '2.0',
-    GLib: '2.0',
-    GObject: '2.0',
-    Gst: '1.0',
-    GstTag: '1.0',
-    Gtk: '4.0',
-    GtkSource: '5',
-    Pango: '1.0',
-    Soup: '3.0',
-    Sushi: '1.0',
-    Gly: '2',
-    GlyGtk4: '2',
-});
-
-const {Gio, GLib} = imports.gi;
 
 const Application = imports.core.application;
 
-function main(argv) {
+export async function main(argv) {
     let application = new Application.Application({ application_id: pkg.name,
                                                     flags: Gio.ApplicationFlags.IS_SERVICE,
                                                     inactivity_timeout: 12000 });
     if (GLib.getenv('SUSHI_PERSIST'))
         application.hold();
-    return application.run(argv);
+    await application.runAsync(argv);
 }
