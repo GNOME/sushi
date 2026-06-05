c970e2f497d5dfcf1d578bef11fc91068e6565c5
Tau Gärtli <git@tau.garden>
2026-06-05 17:17:18 +0200

    ci: Check potfiles

---
+++ build-aux/check-potfiles.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+srcdirs="src"
+desktopdirs="data"
+
+# Blueprint and C source files that contain gettext keywords
+files=$(grep -lR --include='*.blp' --include='*.js' '\(gettext\|[^I_)]_\)(' $srcdirs)
+
+# find .desktop files
+files="$files "$(find $desktopdirs -name '*.desktop*')
+
+# filter out excluded files
+if [ -f po/POTFILES.skip ]; then
+  files=$(for f in $files; do ! grep -q ^$f po/POTFILES.skip && echo $f; done)
+fi
+
+# find those that aren't listed in POTFILES.in
+missing=$(for f in $files; do ! grep -q ^$f po/POTFILES.in && echo $f; done)
+
+if [ ${#missing} -eq 0 ]; then
+  exit 0
+fi
+
+cat >&2 <<EOT
+
+The following files are missing from po/POTFILES.in:
+
+EOT
+for f in $missing; do
+  echo "  $f" >&2
+done
+echo >&2
+
+exit 1
