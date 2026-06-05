eb7fd4d854a59149ee24cf2837672dab4e775ea6
Peter Eisenmann <p3732@getgoogleoff.me>
2026-06-06 00:28:10 +0200

    general: Use SPDX licence abbreviations

--- src/viewers/font.js
+++ src/viewers/font.js
@@ -1,26 +1,7 @@
-/*
- * Copyright (C) 2011 Red Hat, Inc.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- *
- * The Sushi project hereby grant permission for non-gpl compatible GStreamer
- * plugins to be used and distributed together with GStreamer and Sushi. This
- * permission is above and beyond the permissions granted by the GPL license
- * Sushi is covered by.
+/* SPDX-License-Identifier: GPL-2.0-or-later WITH GStreamer-exception-2008
+ * SPDX-FileCopyrightText: 2011 Red Hat, Inc.
  *
  * Authors: Cosimo Cecchi <cosimoc@redhat.com>
- *
  */
 
 import GLib from 'gi://GLib';
