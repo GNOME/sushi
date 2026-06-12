fd2c8e5b836d1ce9e88e066503f53e2c7cd0f0bb
Peter Eisenmann <p3732@getgoogleoff.me>
2026-06-12 21:30:49 +0200

    general: Drop unused constants file

--- src/util/constants.js
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later WITH GStreamer-exception-2008
- * SPDX-FileCopyrightText: 2011 Red Hat, Inc.
- *
- * Authors: Cosimo Cecchi <cosimoc@redhat.com>
- */
-
-export const VIEW_MIN = 340;
-export const TOOLBAR_SPACING = 32;
