8cba92048f58eac4673d9c484af3e7d1f70227b0
Tau Gärtli <git@tau.garden>
2026-07-05 22:15:12 +0200

    html: Handle "open in new window"

--- src/viewers/html.js
+++ src/viewers/html.js
@@ -4,7 +4,13 @@
  * Authors: Cosimo Cecchi <cosimoc@redhat.com>
  */
 
+import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
+
+Gio._promisify(Gtk.UriLauncher.prototype, 'launch', 'launch_finish');
+Gio._promisify(Gtk.FileLauncher.prototype, 'launch', 'launch_finish');
 
 let WebKit;
 try {
@@ -29,6 +35,10 @@ export const Klass = _isAvailable() ? class HTMLRenderer extends WebKit.WebView 
     constructor(file, _fileInfo, constructProperties = {}) {
         super(constructProperties);
 
+        this.cancellable = new Gio.Cancellable();
+
+        this.connect('create', this._onCreate.bind(this));
+
         this.connect('context-menu', this._onContextMenu.bind(this));
         if (pkg.name.endsWith('Devel'))
             this._enableDeveloperExtras();
@@ -38,6 +48,15 @@ export const Klass = _isAvailable() ? class HTMLRenderer extends WebKit.WebView 
             this.emit('error', error);
         });
         this.isReady();
+    }
+
+    /** @param {WebKit.WebView} _webView
+     *  @param {WebKit.NavigationAction} action
+     *  @returns {Gtk.Widget|null} */
+    _onCreate(_webView, action) {
+        const request = action.get_request();
+        this._launchUri(request.get_uri());
+        return null;
     }
 
     /** @param {WebKit.WebView} _webView
@@ -54,6 +73,27 @@ export const Klass = _isAvailable() ? class HTMLRenderer extends WebKit.WebView 
     _enableDeveloperExtras() {
         const settings = this.get_settings();
         settings.set_enable_developer_extras(true);
+    }
+
+    /** @param {string} uri */
+    async _launchUri(uri) {
+        try {
+            await this._tryLaunchUri(uri);
+        } catch (error) {
+            console.warn(`failed to launch URI '${uri}': ${error}`);
+        }
+    }
+
+    /** @param {string} uri */
+    async _tryLaunchUri(uri) {
+        const parent = this.get_root();
+        const isFile = GLib.Uri.parse_scheme(uri).toLowerCase() === 'file';
+        if (isFile) {
+            const file = Gio.File.new_for_uri(uri);
+            await Gtk.FileLauncher.new(file).launch(parent, this.cancellable);
+        } else {
+            await Gtk.UriLauncher.new(uri).launch(parent, this.cancellable);
+        }
     }
 } : undefined;
 
