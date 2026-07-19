7dde72c19cd018ce5c565e4fabe8ec12d6c04e0f
Peter Eisenmann <p3732@getgoogleoff.me>
2026-07-19 15:24:21 +0200

    main-window: Make default size more readable

--- src/core/mainWindow.js
+++ src/core/mainWindow.js
@@ -47,7 +47,8 @@ export class MainWindow extends Adw.ApplicationWindow {
         this._skip_next_size_adjustment = false;
         this._scaled_by_user = false;
 
-        this._lastWindowSize = [min_width, min_height];
+        this._lastWindowWidth = min_width;
+        this._lastWindowHeight = min_height;
         this.set_default_size(min_width, min_height);
 
         this._checkScaledByUser = this._checkScaledByUser.bind(this);
@@ -109,7 +110,7 @@ export class MainWindow extends Adw.ApplicationWindow {
         const height = Math.min(contentSize[1] + naturalTitlebarSize.height, maxSize[1]);
 
         GObject.signal_handlers_block_by_func(this, this._checkScaledByUser);
-        this._setDefaultSize([width, height]);
+        this.#setDefaultSize(width, height);
         GObject.signal_handlers_unblock_by_func(this, this._checkScaledByUser);
     }
 
@@ -128,14 +129,16 @@ export class MainWindow extends Adw.ApplicationWindow {
         this._skip_next_size_adjustment = true;
     }
 
-    _setDefaultSize(windowSize) {
-        if ((windowSize[0] > 0 && windowSize[0] !== this._lastWindowSize[0]) ||
-            (windowSize[1] > 0 && windowSize[1] !== this._lastWindowSize[1])) {
-            if (!this.get_settings().gtk_interface_reduced_motion && this._lastWindowSize[0] !== 0) {
+    #setDefaultSize(width, height) {
+        if ((width > 0 && width !== this._lastWindowWidth) ||
+            (height > 0 && height !== this._lastWindowHeight)) {
+            if (!this.get_settings().gtk_interface_reduced_motion && this._lastWindowWidth !== 0) {
                 const width_target = Adw.PropertyAnimationTarget.new(this, 'default-width');
                 const height_target = Adw.PropertyAnimationTarget.new(this, 'default-height');
-                const width_animation = Adw.TimedAnimation.new(this, this._lastWindowSize[0], windowSize[0], 150, width_target);
-                const height_animation = Adw.TimedAnimation.new(this, this._lastWindowSize[1], windowSize[1], 150, height_target);
+                const width_animation = Adw.TimedAnimation.new(
+                    this, this._lastWindowWidth, width, 150, width_target);
+                const height_animation = Adw.TimedAnimation.new(
+                    this, this._lastWindowHeight, height, 150, height_target);
                 this._animating += 2;
                 [width_animation, height_animation].map(animation => animation.connect_object(
                     'done',
@@ -145,9 +148,10 @@ export class MainWindow extends Adw.ApplicationWindow {
                 width_animation.play();
                 height_animation.play();
             } else {
-                this.set_default_size(...windowSize);
+                this.set_default_size(width, height);
             }
-            this._lastWindowSize = windowSize;
+            this._lastWindowWidth = width;
+            this._lastWindowHeight = height;
         }
     }
 
