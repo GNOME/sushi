442f9b3da67c2d8e4c3b1162985737b6f79f6ffe
Tau Gärtli <git@tau.garden>
2026-08-02 21:03:22 +0200

    image: Play animated images using glycin
    Resolves: <https://gitlab.gnome.org/GNOME/sushi/-/work_items/161>

--- src/viewers/image.js
+++ src/viewers/image.js
@@ -6,15 +6,20 @@
 
 import Adw from 'gi://Adw';
 import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import Gtk from 'gi://Gtk';
 import Gly from 'gi://Gly';
 import GlyGtk4 from 'gi://GlyGtk4';
 
 import {Renderer, ResizePolicy} from '../core/renderer.js';
+import {isCancelledError} from '../util/error.js';
+import {SourceId} from '../util/source.js';
 
 Gio._promisify(Gly.Loader.prototype, 'load_async', 'load_finish');
 Gio._promisify(Gly.Image.prototype, 'next_frame_async', 'next_frame_finish');
+
+const MICROS_PER_MILLIS = 1_000;
 
 export const Klass = class ImageRenderer extends Gtk.Picture {
     static {
@@ -22,6 +27,8 @@ export const Klass = class ImageRenderer extends Gtk.Picture {
             Implements: [Renderer],
         }, this);
     }
+
+    #nextFrameTimeout = new SourceId();
 
     _handleClick(numClicks) {
         if (numClicks === 2)
@@ -50,6 +57,10 @@ export const Klass = class ImageRenderer extends Gtk.Picture {
         this.markInitialized();
     }
 
+    stop() {
+        this.#nextFrameTimeout.remove();
+    }
+
     cleanup() {
         this.set_paintable(null);
     }
@@ -57,13 +68,31 @@ export const Klass = class ImageRenderer extends Gtk.Picture {
     async _loadFile(file) {
         const loader = Gly.Loader.new(file);
         const image = await loader.load_async(this.cancellable);
+        this._image = image;
         this._imageWidth = image.get_width();
         this._imageHeight = image.get_height();
-        const frame = await image.next_frame_async(this.cancellable);
+        await this.#showNextFrame();
+        this.markReady();
+    }
+
+    async #showNextFrame() {
+        const frame = await this._image.next_frame_async(this.cancellable);
         const texture = GlyGtk4.frame_get_texture(frame);
         this._texture = texture;
         this.set_paintable(texture);
-        this.markReady();
+        const delayInMicroseconds = frame.get_delay();
+        if (delayInMicroseconds)
+            this.#queueNextFrame(Math.max(delayInMicroseconds / MICROS_PER_MILLIS, 1));
+    }
+
+    /** @param {number} delayInMilliseconds */
+    #queueNextFrame(delayInMilliseconds) {
+        this.#nextFrameTimeout.timeoutAddOnce(GLib.PRIORITY_DEFAULT, delayInMilliseconds, () => {
+            this.#showNextFrame().catch(error => {
+                if (!isCancelledError(error))
+                    console.warn('Failed to show next image frame', error);
+            });
+        });
     }
 
     vfunc_measure(orientation, _for_size) {
