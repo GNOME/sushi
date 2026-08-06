GNOME/sushi

commit 9b849e76b831a7f52a60f21810fe055ccc4c169b

text: Wrap lines
Fixes https://gitlab.gnome.org/GNOME/sushi/-/work_items/131

src/viewers/text.js
@@ -9,6 +9,7 @@ import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
 import Gtk from 'gi://Gtk';
 import GtkSource from 'gi://GtkSource';
+import Pango from 'gi://Pango';
 
 import {Renderer} from '../core/renderer.js';
 
@@ -35,6 +36,7 @@ export const Klass = class TextRenderer extends Gtk.ScrolledWindow {
             top_margin: 12,
             bottom_margin: 12,
             show_line_numbers: !!buffer.language,
+            wrap_mode: Pango.WrapMode.WORD_CHAR,
         });
 
         this.set_child(this._view);

commit 765899d3f2eeea163012f064dbe3356777143ae9

text: Use Gtk.WrapMode for the text view wrap mode
wrap-mode on GtkTextView is a GtkWrapMode, not a PangoWrapMode.
Pango.WrapMode.WORD_CHAR is 2, which GJS silently accepted as the bare
integer for Gtk.WrapMode.WORD, so long lines without spaces (minified
JS, base64 blobs, URLs) still did not wrap.

The wrong namespace was introduced in commit 9b849e7 ("text: Wrap
lines").

src/viewers/text.js
@@ -10,7 +10,6 @@ import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import Gtk from 'gi://Gtk';
 import GtkSource from 'gi://GtkSource';
-import Pango from 'gi://Pango';
 
 import {Renderer} from '../core/renderer.js';
 
@@ -37,7 +36,7 @@ export const Klass = class TextRenderer extends Gtk.ScrolledWindow {
             top_margin: 12,
             bottom_margin: 12,
             show_line_numbers: !!buffer.language,
-            wrap_mode: Pango.WrapMode.WORD_CHAR,
+            wrap_mode: Gtk.WrapMode.WORD_CHAR,
         });
 
         this.set_child(this._view);