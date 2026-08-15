2c6ecd0440c31ebc1d9d0be5d836a31b47ce85c0
Peter Eisenmann <p3732@getgoogleoff.me>
2026-08-15 13:38:29 +0200

    development: Add cyclic heap dumping mechanism
    Allows to analysis objects on the heap to help find leaks.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -28,3 +28,15 @@ After opening the project in Builder, do the following:
 4. Go to "Application" and change the "Run Command" to your newly created command.
 
 Now you can start Sushi from Builder.
+
+## Find Leaks
+GJS provides a [script to analyse heap dumps](https://gitlab.gnome.org/GNOME/gjs/-/blob/master/tools/heapgraph.md).
+Automatic heap dumping can be activated by setting `SUSHI_DUMP_INTERVAL_S` to an interval at which sushi should do so.
+The heap dumps will be placed in `/tmp/sushi-dumps` and can be analyzed with the `heapgraph.py` script.
+An example that shows the difference between two app states:
+
+```sh
+python3 heapgraph.py --no-gray-roots --no-weak-maps --diff-heap /tmp/sushi-dumps/sushi1.heap /tmp/sushi-dumps/sushi2.heap GObject
+```
+
+More information on memory management in GJS can be found at <https://gjs.guide/guides/gjs/memory-management.html>
--- src/util/development.js
+++ src/util/development.js
@@ -3,6 +3,8 @@
 
 import GLib from 'gi://GLib';
 
+import * as System from 'system';
+
 /** @returns {boolean} */
 export const isRunningFromMesonSource = () =>
     Boolean(GLib.getenv('MESON_BUILD_ROOT') &&
@@ -11,3 +13,29 @@ export const isRunningFromMesonSource = () =>
 /** @returns {string} */
 export const getMesonBuildRoot = () =>
     GLib.getenv('MESON_BUILD_ROOT');
+
+let dumpCounter = 0;
+const dumpDir = GLib.build_filenamev([GLib.get_tmp_dir(), 'sushi-dumps']);
+
+const createDump = () => {
+    console.debug(`Creating dump #${dumpCounter}`);
+    // Run GC to make sure no dangling objects get dumped
+    System.gc();
+    System.dumpHeap(GLib.build_filenamev([dumpDir, `sushi${dumpCounter}.heap`]));
+    dumpCounter += 1;
+    return GLib.SOURCE_CONTINUE;
+};
+
+/** Initializes periodic heap dumps if SUSHI_DUMP_INTERVAL_S is set. */
+export const checkInitDumping = () => {
+    const intervalSeconds = GLib.getenv('SUSHI_DUMP_INTERVAL_S');
+    if (intervalSeconds) {
+        console.warn(`Heap dumps will be created every ${intervalSeconds} seconds in ${dumpDir} (SUSHI_DUMP_INTERVAL_S is set)`);
+        GLib.mkdir_with_parents(dumpDir, 0o755);
+        GLib.timeout_add(
+            GLib.G_PRIORITY_DEFAULT,
+            intervalSeconds * 1000,
+            createDump
+        );
+    }
+};
