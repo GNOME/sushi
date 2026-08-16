caa8d3397515c912fd9a1e98aaa1046f2629c1a6
Peter Eisenmann <p3732@getgoogleoff.me>
2026-08-17 01:44:47 +0200

    pdf: Separate out Libreoffice conversion logic
    Reduces the Libreoffice snippets in pdf.js to a minimum.

---
+++ src/viewers/libreoffice-pdf.js
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0-or-later WITH GStreamer-exception-2008
+// SPDX-FileCopyrightText: 2026 The Sushi authors
+
+import Sushi from 'gi://Sushi';
+
+import {Klass as PdfRenderer} from './pdf.js';
+import * as Libreoffice from './libreoffice.js';
+
+PdfRenderer.convertLibreoffice = (renderer, file) => {
+    Sushi.convert_libreoffice(file, renderer.cancellable, (o, res) => {
+        try {
+            const convertedFile = Sushi.convert_libreoffice_finish(res);
+            renderer._loadFile(convertedFile);
+        } catch (e) {
+            renderer.emit('error', e);
+        }
+    });
+};
+
+export const Klass = PdfRenderer;
+
+export const mimeTypes = Libreoffice.isAvailable() ? [] : Libreoffice.officeTypes;
