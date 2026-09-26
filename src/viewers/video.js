489cb7505ac84849523852bcac62a87fa6566be0
Peter Eisenmann <p3732@getgoogleoff.me>
2026-09-26 11:36:21 +0200

    video: Also mark failed without an error

--- src/viewers/video.js
+++ src/viewers/video.js
@@ -57,8 +57,7 @@ export const Klass = class VideoRenderer extends Adw.Bin {
         });
         this.#errorHandleId = this._stream.connect(
             'notify::error', mediaFile => {
-                if (mediaFile.error)
-                    this.markFailed(mediaFile.error);
+                this.markFailed(mediaFile.error ?? null);
             });
 
         this.markInitialized();
